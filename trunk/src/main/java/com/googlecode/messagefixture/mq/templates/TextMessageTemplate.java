3de2d21b4f6ce2cfe1ee8ca9836365b32414e27c
johannescarlen <johannescarlen@eeeceecc-163a-0410-afa0-e5be7276e887>
2007-12-12 12:18:33 +0000

    Added mask metod to mq text template as well

--- trunk/src/main/java/com/googlecode/messagefixture/mq/templates/TextMessageTemplate.java
+++ trunk/src/main/java/com/googlecode/messagefixture/mq/templates/TextMessageTemplate.java
@@ -45,4 +45,22 @@ public class TextMessageTemplate extends MessageTemplate {
 			return new byte[0];
 		}
 	}
+	
+	public boolean mask(String expected, String maskCharacter){
+		char maskChar = maskCharacter.charAt(0);
+		
+		if(expected.length()!=text.length())
+			return false;
+		
+		for(int pos=0;pos<text.length();pos++){
+			char c = expected.charAt(pos); 
+			if(c!=maskChar){
+				if(c!=text.charAt(pos)){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
 }
